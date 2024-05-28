fec0cf937638e137e7ad114a5c255e0e47f9977c
Gellila Zeru <gellilazeru1@gmail.com>
2024-05-28 12:33:49 +0100

    customer complete

--- travelagency_PP/src/main/java/Customer.java
+++ travelagency_PP/src/main/java/Customer.java
@@ -1,4 +1,50 @@
 public class Customer {
 
-    hello
+    private String name;
+    private int wallet;
+
+    public Customer (String name){
+        this.name = name;
+        this.wallet = 100;
+    }
+
+    // GETTERS
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getWallet(){
+        return this.wallet;
+    }
+
+    // SETTERS
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setWallet(int wallet){
+        this.wallet = wallet;
+    }
+
+    // METHODS
+
+    // to pay - there is an amount coming out of your wallet
+    // check if they can afford
+
+    public boolean checkIfCanAfford(int amount){
+        if(this.wallet >= amount){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public void pay(int amount){
+        this.wallet -= amount;
+    }
+
+
 }
---
+++ travelagency_PP/src/test/java/CustomerTest.java
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CustomerTest {
+
+    Customer customer;
+
+    @BeforeEach
+    public void setUp(){
+        customer = new Customer("Faith");
+
+    }
+
+    @Test
+    public void canGetName (){
+        assertThat(customer.getName()).isEqualTo("Faith");
+    }
+
+    @Test
+    public void canGetWallet (){
+        assertThat(customer.getWallet()).isEqualTo(100);
+    }
+
+    @Test
+    public void canSetName(){
+        this.customer.setName("Gellila");
+        assertThat(customer.getName()).isEqualTo("Gellila");
+    }
+
+    @Test
+    public void canSetWallet(){
+        this.customer.setWallet(50);
+        assertThat(customer.getWallet()).isEqualTo(50);
+    }
+
+    @Test
+    public void canCheckIfCanAfford(){
+        assertThat(customer.checkIfCanAfford(67)).isEqualTo(true);
+
+    }
+
+    @Test
+    public void canPay(){
+        customer.pay(60);
+        int result = customer.getWallet();
+        assertThat(result).isEqualTo(40);
+    }
+
+}
